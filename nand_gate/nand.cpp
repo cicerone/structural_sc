d5f8adf73136fc71a44a80bee7fa7f9871e9450d
roni <cicerone.mihalache@kotys.biz>
2013-10-07 20:43:28 -0700

    Cleanup.

--- nand_gate/nand.cpp
+++ nand_gate/nand.cpp
@@ -1,88 +1,86 @@
-#include "systemc.h"
-#include <iomanip>      // std::setw
+/* ===============================================================================================
+Copyright (c) 2009 Kotys LLC. Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================================== */
 
-using namespace std;
+#include "systemc.h"
+
 
 SC_MODULE(Nand2)                   // declare Nand2 sc_module
 {
-    sc_in<bool> In1_in, In2_in;    // input signal ports
+    sc_in<bool> in1_in, in2_in;    // input signal ports
     sc_out<bool> Out_out;          // output signal ports
     
     void ComputeNand()      
     {
-        Out_out.write( !(In1_in.read() && In2_in.read()) );
+        Out_out.write( !(in1_in.read() && in2_in.read()) );
     }
     
     SC_CTOR(Nand2)                      // constructor for Nand2
     {
         SC_METHOD(ComputeNand);         // register do_nand2 with kernel
-        sensitive << In1_in << In2_in;  // sensitivity list
+        sensitive << in1_in << in2_in;  // sensitivity list
     }
 };
 
-// show this diff
 
 SC_MODULE(TestGenerator)
 {
-    sc_out<bool> In1_out, In2_out;
-    sc_in<bool>  Clk;
+    sc_out<bool> in1_out, in2_out;
+    sc_in<bool>  clk;
     
     void GenerateSignals()
     {
         while(1)
         {
-            In1_out.write(false);
-            In2_out.write(false);
+            in1_out.write(false);
+            in2_out.write(false);
             wait();
-            In1_out.write(false);
-            In2_out.write(true);
+            in1_out.write(false);
+            in2_out.write(true);
             wait();
-            In1_out.write(true);
-            In2_out.write(false);
+            in1_out.write(true);
+            in2_out.write(false);
             wait();
-            In1_out.write(true);
-            In2_out.write(true);
+            in1_out.write(true);
+            in2_out.write(true);
             wait();
         }
     }
     
     SC_CTOR(TestGenerator)
     {
-        SC_THREAD(GenerateSignals);
-        sensitive << Clk.pos();
+        SC_CTHREAD(GenerateSignals, clk.pos());
     }
 };
 
 
-
 int sc_main(int argc, char* argv[])
 {
-  sc_signal<bool> ASig, BSig, FSig;
-  sc_clock TestClk("TestClock", 10, SC_NS,0.5);
+    sc_signal<bool> in1_sig, in2_sig, out_sig;
+    sc_clock clk_sig("TestClock", 10, SC_NS,0.5);
 
+    TestGenerator tg("test_generator");
+    tg.in1_out(in1_sig);
+    tg.in2_out(in2_sig);
+    tg.clk(clk_sig);
 
-  TestGenerator Stim1("Stimulus");
-  Stim1.In1_out(ASig);
-  Stim1.In2_out(BSig);
-  Stim1.Clk(TestClk);
+    Nand2 DUT("NAND2");
+    DUT.in1_in(in1_sig);
+    DUT.in2_in(in2_sig);
+    DUT.Out_out(out_sig);
 
-  Nand2 DUT("NAND2");
-  DUT.In1_in(ASig);
-  DUT.In2_in(BSig);
-  DUT.Out_out(FSig);
+    sc_trace_file* p_trace_file;
+    p_trace_file = sc_create_vcd_trace_file("traces");
+    sc_trace(p_trace_file, in1_sig  , "in1" );
+    sc_trace(p_trace_file, in2_sig  , "in2" );
+    sc_trace(p_trace_file, out_sig  , "out" );
+    sc_trace(p_trace_file, clk_sig  , "clk" );
 
-  sc_trace_file* Tf;
-  Tf = sc_create_vcd_trace_file("traces");
- // ((vcd_trace_file*)Tf)->sc_set_vcd_time_unit(-9);
-  sc_trace(Tf, ASig  , "In1" );
-  sc_trace(Tf, BSig  , "In2" );
-  sc_trace(Tf, FSig  , "Out" );
+    sc_start(70, SC_NS); 
+    sc_close_vcd_trace_file(p_trace_file);
 
-  sc_start(70, SC_NS); 
-  sc_close_vcd_trace_file(Tf);
-
-  return 0;
-
+    return 0;
 }
 
 
